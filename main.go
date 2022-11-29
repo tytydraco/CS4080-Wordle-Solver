6b5e13a49b47db295d5c66ea94d8ea8d056b1aab
Tyler Nijmeh <tylernij@gmail.com>
2022-11-29 12:33:20 -0800

    main: Read valid words into variable
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func read_dictionary() []string {
+	readFile, _ := os.Open("words.txt")
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	var fileLines []string
+	for fileScanner.Scan() {
+		fileLines = append(fileLines, fileScanner.Text())
+	}
+
+	readFile.Close()
+
+	return fileLines
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	words := read_dictionary()
+	fmt.Println(words)
 }
