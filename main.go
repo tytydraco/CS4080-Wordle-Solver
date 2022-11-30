90044a1ddb1760eeb0eca1e1485fd749f5948087
Tyler Nijmeh <tylernij@gmail.com>
2022-11-29 18:15:58 -0800

    main: Retrieve user feedback for test guess
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+const WORD_LEN = 5
 
 func GetValidWordList() []string {
 	readFile, _ := os.Open("words.txt")
@@ -21,7 +24,25 @@ func GetValidWordList() []string {
 	return fileLines
 }
 
+func GetWordFeedback(word string) {
+	letters := strings.Split(word, "")
+	feedback := new([WORD_LEN]string)
+
+	fmt.Printf(" --- Feedback for %s --- \n", word)
+	for i, v := range letters {
+		var letterFeedback string
+		fmt.Printf("%s: ", v)
+		fmt.Scanf("%s", &letterFeedback)
+
+		feedback[i] = letterFeedback
+	}
+
+	fmt.Println(feedback)
+}
+
 func main() {
 	words := GetValidWordList()
 	fmt.Println(words)
+
+	GetWordFeedback(words[0])
 }
