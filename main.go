f6c3351cf61ab0cae44f5d48584fb4c3ac58dee1
INSORTIAL <renwell2099@gmail.com>
2022-12-03 21:49:28 -0800

    Made changes to getWordFeedback and implemented RemoveInvalidWords

--- main.go
+++ main.go
@@ -34,6 +34,9 @@ var (
 // List of valid words to choose from
 var validWords []string
 
+// Contains a map of unordered words to choose from
+var unorderedWords map[string]int
+
 func GetValidWordList() []string {
 	readFile, _ := os.Open("words.txt")
 	fileScanner := bufio.NewScanner(readFile)
@@ -103,14 +106,18 @@ func GetWordFeedback(word string) []Feedback {
 	for i, v := range letters {
 		var letterFeedbackStr string
 		fmt.Printf("%s: ", v)
-		fmt.Scanf("%s", &letterFeedbackStr)
+		fmt.Scanf("%s\n", &letterFeedbackStr)
 
-		letterFeedback, exists := feedbackMap[letterFeedbackStr]
+		letterFeedback, isPresent := feedbackMap[letterFeedbackStr]
 
 		// Todo: make this better
-		if !exists {
-			fmt.Println("Bad feedback!")
-			os.Exit(1)
+		for !isPresent {
+			fmt.Println("Bad feedback! Try again.")
+			fmt.Printf("%s: ", v)
+			fmt.Scanf("%s\n", &letterFeedbackStr)
+
+			_, valid := feedbackMap[letterFeedbackStr]
+			isPresent = valid
 		}
 
 		feedback[i] = letterFeedback
@@ -119,12 +126,81 @@ func GetWordFeedback(word string) []Feedback {
 	return feedback
 }
 
-func RemoveInvalidWords(feedback []Feedback) int {
+func RemoveInvalidWords(feedback []Feedback, bestGuess string) int {
 	// TODO: Go through all feedbacks (right place, wrong place, or not exists)
 	//		 and remove words in the validWords that aren't possible answers.
 	//		 Return the number of entries eliminated.
+	guessLetters := strings.Split(bestGuess, "")
+	nonexistentLetters := make(map[string]int)
+	invalidWordsMap := make(map[string]int)
 
-	return 0
+	var invalidWords int = 0
+
+	//	Create a nonexistent letter map
+	for j, letter := range feedback {
+		if letter == Nonexistent {
+			nonexistentLetters[guessLetters[j]] = 0
+		}
+	}
+
+	//	Goes through list of validWords, iterates through each character
+	//	If any do not fit the feedback, it is added to the invalidWordsMap
+	for _, v := range validWords {
+		var unorderedWordCount int = 0
+		validLetters := strings.Split(v, "")
+		for j := range feedback {
+			_, nonexistent := nonexistentLetters[validLetters[j]]
+			_, letterIsPresent := unorderedWords[validLetters[j]]
+
+			// Checks if the correct characters are in the correct place.
+			if feedback[j] == Correct && validLetters[j] != guessLetters[j] {
+				invalidWordsMap[v] = 0
+				invalidWords++
+				break
+			}
+
+			//	Checks if word has unordered characters
+			if validLetters[j] == guessLetters[j] && feedback[j] == Unordered {
+				invalidWordsMap[v] = 0
+				invalidWords++
+				if !letterIsPresent {
+					unorderedWords[validLetters[j]] = 0
+				}
+				break
+			}
+
+			//	Checks if word contains nonexistent characters
+			if nonexistent {
+				invalidWordsMap[v] = 0
+				invalidWords++
+				break
+			}
+
+			// Checks if a nonexistent character is in the word
+			if letterIsPresent {
+				unorderedWordCount++
+			}
+		}
+
+		if unorderedWordCount < len(unorderedWords) && len(unorderedWords) != 0 {
+			invalidWordsMap[v] = 0
+			invalidWords++
+		}
+	}
+
+	// Checks if word from validWords array exists in invalidWordsMap.
+	// If not it does not add it to newValidWords array
+	var newValidWords []string
+	for _, v := range validWords {
+		_, isPresent := invalidWordsMap[v]
+
+		if !isPresent {
+			newValidWords = append(newValidWords, v)
+		}
+	}
+
+	validWords = newValidWords
+	return invalidWords
 }
 
 // ChooseNextBestGuess calculates the highest score from possibleWords and returns
@@ -147,6 +223,7 @@ func ChooseNextBestGuess(possibleWords []string, frequencies []map[byte]int) str
 
 func main() {
 	validWords = GetValidWordList()
+	unorderedWords = make(map[string]int)
 	fmt.Println(len(validWords))
 
 	for i := 0; i < NUM_TRIES; i++ {
@@ -157,7 +234,7 @@ func main() {
 		feedback := GetWordFeedback(nextBestGuess)
 		// TODO: If all correct, we win!
 
-		removed := RemoveInvalidWords(feedback)
+		removed := RemoveInvalidWords(feedback, nextBestGuess)
 		// TODO: Also remove our guess
 		fmt.Printf("Eliminated %d words!\n", removed)
 		fmt.Println()
